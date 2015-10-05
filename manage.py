d0ee94d6fa2de4bb6b73a17a4549a058c807f1d7
Alair J Tavares <alairjt@gmail.com>
2015-10-04 21:47:15 -0300

    Heroku

--- manage.py
+++ manage.py
@@ -1,10 +1,33 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import os
-import sys
+from os import environ as env
+from sys import argv
 
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
+import bottle
+from bottle import default_app, request, route, response, get
 
-    from django.core.management import execute_from_command_line
+bottle.debug(True)
 
-    execute_from_command_line(sys.argv)
+@get('/')
+def index():
+    response.content_type = 'text/plain; charset=utf-8'
+    ret =  'Hello world, I\'m %s!\n\n' % os.getpid()
+    ret += 'Request vars:\n'
+    for k, v in request.environ.iteritems():
+        if 'bottle.' in k:
+            continue
+        ret += '%s=%s\n' % (k, v)
+
+    ret += '\n'
+    ret += 'Environment vars:\n'
+
+    for k, v in env.iteritems():
+        if 'bottle.' in k:
+            continue
+        ret += '%s=%s\n' % (k, v)
+
+    return ret
+
+bottle.run(host='0.0.0.0', port=argv[1])
--- src/main.py
+++
@@ -1,9 +0,0 @@
-from bottle import route, run
-import requests
-
-@route('/')
-def find():
-    r = requests.get('http://www.omdbapi.com/?t=hannibal&y=&plot=short&r=json')
-    return r.json()
-
-run(host='localhost', port=8080, debug=True)
